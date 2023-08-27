6f80d490aba2601b4934012363b68bbb0f701bca
Avirup Mazumder <80705801+AvirupMazumder@users.noreply.github.com>
2023-08-28 00:28:45 +0530

    Update solution.cpp

--- Tree/BST/Valid BST from Preorder/solution.cpp
+++ Tree/BST/Valid BST from Preorder/solution.cpp
@@ -1,3 +1,20 @@
+/*
+    The idea is to use a stack. This problem is similar to Next (or closest) Greater Element problem.
+Here we find the next greater element and after finding next greater, if we find a smaller element, then return false.
+
+    1. Create an empty stack.
+    2. Initialize root as INT_MIN.
+    3. Do following for every element pre[i]
+       a. If pre[i] is smaller than current root, return false.
+       b.  Keep removing elements from stack while pre[i] is greater
+        then stack top. Make the last removed item as new root (to
+        be compared next).
+        At this point, pre[i] is greater than the removed root
+        (That is why if we see a smaller element in step a), we
+        return false)
+       c. push pre[i] to stack (All elements in stack are in decreasing
+        order)
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int canRepresentBST(vector<int> pre)
