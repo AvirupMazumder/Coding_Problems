5a42d04620262ff5baff763340fb6df8b1845960
Avirup Mazumder <80705801+AvirupMazumder@users.noreply.github.com>
2023-08-28 23:56:44 +0530

    updated counting_subarrays

--- 2 Pointer/Counting_Subarrays/solution.cpp
+++ 2 Pointer/Counting_Subarrays/solution.cpp
@@ -24,3 +24,15 @@ int solve(vector<int> &A, int B) {
 
 int main()
 {
+    int n;
+    cin>>n;
+    vector<int> A(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>A[i];
+    }
+    int B;
+    cin>>B;
+    cout<<solve(A,B)<<endl;
+    return 0;
+}
